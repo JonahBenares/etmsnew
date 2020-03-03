58547dce1da37afd50d7b0bc1c83618d37ea89f3
ProjectPixel <jasonflor.cenpri@gmail.com>
2020-03-03 15:31:01 +0800

    history

--- application/views/report/history_view.php
+++ application/views/report/history_view.php
@@ -1,5 +1,6 @@
 <script src="<?php echo base_url(); ?>assets/dist/js/jquery.js"></script>
 <script src="<?php echo base_url(); ?>assets/dist/js/encode.js"></script>
+<link  href="<?php echo base_url(); ?>assets/dist/css/history.css" rel="stylesheet">
 
 <div class="page-wrapper">
     <div class="container-fluid ">
@@ -13,334 +14,96 @@
             </div>
             <div class="col-md-2 align-self-center text-right"></div>
         </div>
-        <!-- <style type="text/css">
-            header h1{
-              text-align: center;
-              font-weight: bold;
-              margin-top: 0;
-            }
-              
-             header p{
-               text-align: center;
-               margin-bottom: 0;
-             }
-
-            .hexa{
-              border: 0px;
-              float: left;
-              text-align: center;
-              height: 35px;
-              width: 60px;
-              font-size: 22px;
-              background: #f0f0f0;
-              color: #3c3c3c;
-              position: relative;
-              margin-top: 15px;
-            }
-
-            .hexa:before{
-              content: ""; 
-              position: absolute; 
-              left: 0; 
-              width: 0; 
-              height: 0;
-              border-bottom: 15px solid #f0f0f0;
-              border-left: 30px solid transparent;
-              border-right: 30px solid transparent;
-              top: -15px;
-            }
-
-            .hexa:after{
-              content: ""; 
-              position: absolute; 
-              left: 0; 
-              width: 0; 
-              height: 0;
-              border-left: 30px solid transparent;
-              border-right: 30px solid transparent;
-              border-top: 15px solid #f0f0f0;
-              bottom: -15px;
-            }
-
-            .timeline {
-              position: relative;
-              padding: 0;
-              width: 100%;
-              margin-top: 20px;
-              list-style-type: none;
-            }
-
-            .timeline:before {
-              position: absolute;
-              left: 50%;
-              top: 0;
-              content: ' ';
-              display: block;
-              width: 2px;
-              height: 100%;
-              margin-left: -1px;
-              background: rgb(213,213,213);
-              background: -moz-linear-gradient(top, rgba(213,213,213,0) 0%, rgb(213,213,213) 8%, rgb(213,213,213) 92%, rgba(213,213,213,0) 100%);
-              background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(30,87,153,1)), color-stop(100%,rgba(125,185,232,1)));
-              background: -webkit-linear-gradient(top, rgba(213,213,213,0) 0%, rgb(213,213,213) 8%, rgb(213,213,213) 92%, rgba(213,213,213,0) 100%);
-              background: -o-linear-gradient(top, rgba(213,213,213,0) 0%, rgb(213,213,213) 8%, rgb(213,213,213) 92%, rgba(213,213,213,0) 100%);
-              background: -ms-linear-gradient(top, rgba(213,213,213,0) 0%, rgb(213,213,213) 8%, rgb(213,213,213) 92%, rgba(213,213,213,0) 100%);
-              background: linear-gradient(to bottom, rgba(213,213,213,0) 0%, rgb(213,213,213) 8%, rgb(213,213,213) 92%, rgba(213,213,213,0) 100%);
-              z-index: 5;
-            }
-
-            .timeline li {
-              padding: 2em 0;
-            }
-
-            .timeline .hexa{
-              width: 16px;
-              height: 10px;
-              position: absolute;
-              background: #00c4f3;
-              z-index: 5;
-              left: 0;
-              right: 0;
-              margin-left:auto;
-              margin-right:auto;
-              top: -30px;
-              margin-top: 0;
-            }
-
-            .timeline .hexa:before {
-              border-bottom: 4px solid #00c4f3;
-              border-left-width: 8px;
-              border-right-width: 8px;
-              top: -4px;
-            }
-
-            .timeline .hexa:after {
-              border-left-width: 8px;
-              border-right-width: 8px;
-              border-top: 4px solid #00c4f3;
-              bottom: -4px;
-            }
-
-            .direction-l,
-            .direction-r {
-              float: none;
-              width: 100%;
-              text-align: center;
-            }
-
-            .flag-wrapper {
-              text-align: center;
-              position: relative;
-            }
-
-            .flag {
-              position: relative;
-              display: inline;
-              background: rgb(255,255,255);
-              font-weight: 600;
-              z-index: 15;
-              padding: 6px 10px;
-              text-align: left;
-              border-radius: 5px;
-            }
-
-            .direction-l .flag:after,
-            .direction-r .flag:after {
-              content: "";
-              position: absolute;
-              left: 50%;
-              top: -15px;
-              height: 0;
-              width: 0;
-              margin-left: -8px;
-              border: solid transparent;
-              border-bottom-color: rgb(255,255,255);
-              border-width: 8px;
-              pointer-events: none;
-            }
-
-            .direction-l .flag {
-              -webkit-box-shadow: -1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
-              -moz-box-shadow: -1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
-              box-shadow: -1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
-            }
-
-            .direction-r .flag {
-              -webkit-box-shadow: 1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
-              -moz-box-shadow: 1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
-              box-shadow: 1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
-            }
-
-            .time-wrapper {
-              display: block;
-              position: relative;
-              margin: 4px 0 0 0;
-              z-index: 14;
-              line-height: 1em;
-              vertical-align: middle;
-              color: #fff;
-            }
-
-            .direction-l .time-wrapper {
-              float: none;
-            }
-
-            .direction-r .time-wrapper {
-              float: none;
-            }
-
-            .time {
-              background: #00c4f3;
-              display: inline-block;
-              padding: 8px;
-            }
-
-            .desc {
-              position: relative;
-              margin: 1em 0 0 0;
-              padding: 1em;
-              background: rgb(254,254,254);
-              -webkit-box-shadow: 0 0 1px rgba(0,0,0,0.20);
-              -moz-box-shadow: 0 0 1px rgba(0,0,0,0.20);
-              box-shadow: 0 0 1px rgba(0,0,0,0.20);
-              z-index: 15;
-            }
-
-            .direction-l .desc,
-            .direction-r .desc {
-              position: relative;
-              margin: 1em 1em 0 1em;
-              padding: 1em;
-              z-index: 15;
-            }
-
-            @media(min-width: 768px){
-              .timeline {
-                width: 660px;
-                margin: 0 auto;
-                margin-top: 20px;
-              }
-
-              .timeline li:after {
-                content: "";
-                display: block;
-                height: 0;
-                clear: both;
-                visibility: hidden;
-              }
-              
-              .timeline .hexa {
-                left: -28px;
-                right: auto;
-                top: 8px;
-              }
-
-              .timeline .direction-l .hexa {
-                left: auto;
-                right: -28px;
-              }
-
-              .direction-l {
-                position: relative;
-                width: 310px;
-                float: left;
-                text-align: right;
-              }
-
-              .direction-r {
-                position: relative;
-                width: 310px;
-                float: right;
-                text-align: left;
-              }
-
-              .flag-wrapper {
-                display: inline-block;
-              }
-              
-              .flag {
-                font-size: 18px;
-              }
-
-              .direction-l .flag:after {
-                left: auto;
-                right: -16px;
-                top: 50%;
-                margin-top: -8px;
-                border: solid transparent;
-                border-left-color: rgb(254,254,254);
-                border-width: 8px;
-              }
-
-              .direction-r .flag:after {
-                top: 50%;
-                margin-top: -8px;
-                border: solid transparent;
-                border-right-color: rgb(254,254,254);
-                border-width: 8px;
-                left: -8px;
-              }
-
-              .time-wrapper {
-                display: inline;
-                vertical-align: middle;
-                margin: 0;
-              }
-
-              .direction-l .time-wrapper {
-                float: left;
-              }
-
-              .direction-r .time-wrapper {
-                float: right;
-              }
-
-              .time {
-                padding: 5px 10px;
-              }
-
-              .direction-r .desc {
-                margin: 1em 0 0 0.75em;
-              }
-            }
-
-            @media(min-width: 992px){
-              .timeline {
-                width: 800px;
-                margin: 0 auto;
-                margin-top: 20px;
-              }
-
-              .direction-l {
-                position: relative;
-                width: 380px;
-                float: left;
-                text-align: right;
-              }
-
-              .direction-r {
-                position: relative;
-                width: 380px;
-                float: right;
-                text-align: left;
-              }
-            }
-        </style>
-        <header>
-            <p>Worked on all modern browers</p>
-            <h1>CSS based responsive timeline</h1>
-        </header>
+        <center>
+            <div class="card shadow-sn m-l-150 m-r-150">
+                <div class="card-body">
+                    <div class="col-lg-8">
+                        <h3><?php echo $item ?></h3>
+                    </div>
+                    <div class="col-lg-4"></div>
+                    
+                    <br>
+                    <table class= "">
+                        <tr>
+                            <td align="center"><h4><?php echo $brand ?></h4></td>
+                            <td><h4 class="m-l-5 m-r-20">-</h4></td>
+                            <td align="center"><h4><?php echo $model ?></h4></td>
+                            <td><h4 class="m-l-5 m-r-20">-</h4></td>
+                            <td align="center"><h4><?php echo $sn ?></h4></td>
+                            <td><h4 class="m-l-5 m-r-20"></h4></td>
+                        </tr>
+                        <tr>
+                            <td style="border-top:1px solid #d7d7d7"><center><small>Brand</small></center></td>
+                            <td></td>
+                            <td style="border-top:1px solid #d7d7d7"><center><small>Model</small></center></td>
+                            <td></td>
+                            <td style="border-top:1px solid #d7d7d7"><center><small>Serial Number</small></center></td>
+                        </tr>
+                        <?php if($damage == '1'){ ?>
+                        <tr>
+                            <td colspan="7"><button class="btn btn-danger btn-block btn-lg animated headShake">DAMAGED</button></td>
+                        </tr>
+                        <?php }else if($ids==0 && $changeloc==0){ ?>
+                        <tr>
+                            <td colspan="7"><button class="btn btn-success btn-block btn-lg animated headShake">Available</button></td>
+                        </tr>
+                        <?php } else if($borrowed=='1'){ ?>
+                        <tr>
+                            <td colspan="7"><button class="btn btn-warning btn-block btn-lg animated headShake text-white">Borrowed</button></td>
+                        </tr>   
+                        <?php } else if($ids==0 && $changeloc=='1'){ ?>
+                        <tr>
+                            <td colspan="7"><button class="btn btn-info btn-block btn-lg animated headShake text-white">Moved to <?php echo $location; ?></button></td>
+                        </tr> 
+                        <?php } else if($lost=='1'){ ?>
+                        <tr>
+                            <td colspan="7"><button class="btn btn-secondary btn-block btn-lg animated headShake text-white">Lost Item</button></td>
+                        </tr> 
+                        <?php }else { echo ''; } ?>
+                    </table>
+                </div>
+            </div>
+        </center>
 
         <ul class="timeline">
 
             <li>
+            <div class="direction-r">
+            <div class="flag-wrapper">
+            <span class="hexa"></span>
+            <span class="flag">Currrent</span>
+            <span class="time-wrapper"><span class="time bg-info-alt">Feb 2015</span></span>
+            </div>
+            <div class="desc shadow-sn border-info">
+                <p class="font-bold">Employee Name:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Return Date:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Received By:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Qty:</p>
+                <p class="m-b-10">lorem ipsum</p>
+            </div>
+            </div>
+            </li>
+
+            <li>
             <div class="direction-l">
             <div class="flag-wrapper">
             <span class="hexa"></span>
-            <span class="flag">Lorem ipsum Anim.</span>
-            <span class="time-wrapper"><span class="time">Dec 2014</span></span>
+            <span class="flag">Returned</span>
+            <span class="time-wrapper"><span class="time bg-success-alt">Dec 2014</span></span>
             </div>
-            <div class="desc">Lorem ipsum In ut sit in dolor nisi ex magna eu anim anim tempor dolore aliquip enim cupidatat laborum dolore.</div>
+            <div class="desc shadow-sn border-success">
+                <p class="font-bold">Employee Name:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Return Date:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Received By:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Qty:</p>
+                <p class="m-b-10">lorem ipsum</p>
+            </div>
             </div>
             </li>
 
@@ -349,10 +112,19 @@
             <div class="direction-r">
             <div class="flag-wrapper">
             <span class="hexa"></span>
-            <span class="flag">Lorem ipsum.</span>
-            <span class="time-wrapper"><span class="time">Feb 2015</span></span>
+            <span class="flag">Borrowed</span>
+            <span class="time-wrapper"><span class="time bg-warning-alt">Feb 2015</span></span>
             </div>
-            <div class="desc">Lorem ipsum Nisi labore aute do aute culpa magna nulla voluptate exercitation deserunt proident.</div>
+            <div class="desc shadow-sn border-warning">
+                <p class="font-bold">Employee Name:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Return Date:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Received By:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Qty:</p>
+                <p class="m-b-10">lorem ipsum</p>
+            </div>
             </div>
             </li>
 
@@ -360,10 +132,23 @@
             <div class="direction-l">
             <div class="flag-wrapper">
             <span class="hexa"></span>
-            <span class="flag">Lorem ipsum Anim.</span>
-            <span class="time-wrapper"><span class="time">Dec 2014</span></span>
+            <span class="flag">Repaired</span>
+            <span class="time-wrapper"><span class="time bg-danger-alt">Dec 2014</span></span>
             </div>
-            <div class="desc">Lorem ipsum In ut sit in dolor nisi ex magna eu anim anim tempor dolore aliquip enim cupidatat laborum dolore.</div>
+            <div class="desc shadow-sn border-danger">
+                <p class="font-bold">JO No.:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Repair Price:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Supplier:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Received By :</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Repair Date:</p>
+                <p class="m-b-10">lorem ipsum</p>
+                <p class="font-bold">Qty:</p>
+                <p class="m-b-10">lorem ipsum</p>
+            </div>
             </div>
             </li>
 
@@ -371,13 +156,24 @@
             <div class="direction-r">
             <div class="flag-wrapper">
             <span class="hexa"></span>
-            <span class="flag">Lorem Occaecat.</span>
-            <span class="time-wrapper"><span class="time">July 2014</span></span>
+            <span class="flag">Lost</span>
+            <span class="time-wrapper "><span class="time bg-dark-alt">July 2014</span></span>
             </div>
-            <div class="desc">Lorem ipsum Minim labore Ut cupidatat quis qui deserunt proident fugiat pariatur cillum cupidatat reprehenderit sit id dolor consectetur ut.</div>
+            <div class="desc shadow-sn border-dark">
+               <p class="font-bold">Date Lost:</p>
+                <p class="m-b-10">lorem ipsum</p>
+               <p class="font-bold">Item:</p>
+                <p class="m-b-10">lorem ipsum</p>
+               <p class="font-bold">Replacement Item:</p>
+                <p class="m-b-10">lorem ipsum</p>
+               <p class="font-bold">Accountable Person:</p>
+                <p class="m-b-10">lorem ipsum</p>
+               <p class="font-bold">Remarks:</p>
+                <p class="m-b-10">lorem ipsum</p>
+            </div>
             </div>
             </li>
-        </ul> -->
+        </ul>
 
 
         <div class="row">
